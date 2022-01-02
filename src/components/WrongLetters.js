03ef162d328f98ddb61a386f92f3e3145199d279
areeb vohra <areebvohra@gmail.com>
2022-01-02 18:16:28 +0500

    comments added

--- src/components/WrongLetters.js
+++ src/components/WrongLetters.js
@@ -9,9 +9,11 @@ const WrongLetters = ({ wrongLetters }) => {
         {wrongLetters.length > 0 &&
           <p>Wrong</p>
         }
-        {wrongLetters
-          .map((letter, i) => <span key={i}>{letter}</span>)
-          .reduce((prev, curr) => prev === null ? [curr] : [prev, ', ', curr], null)}
+        {
+          wrongLetters
+            .map((letter, i) => <span key={i}>{letter}</span>) // The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.  
+            .reduce((prev, curr) => prev === null ? [curr] : [prev, ', ', curr], null) // The reduce() method executes a user-supplied “reducer” callback function on each element of the array, in order, passing in the return value from the calculation on the preceding element. The final result of running the reducer across all elements of the array is a single value. 
+        }
       </div>
     </div>
   )
